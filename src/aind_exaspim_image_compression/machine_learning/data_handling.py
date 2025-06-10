31b35df6f78d9db03f85f94790e476181af9fb97
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-06-09 19:17:50 -0700

    refactor: simplified funcationality

--- src/aind_exaspim_image_compression/machine_learning/data_handling.py
+++ src/aind_exaspim_image_compression/machine_learning/data_handling.py
@@ -15,12 +15,12 @@ from concurrent.futures import (
     ThreadPoolExecutor,
     as_completed,
 )
+from copy import deepcopy
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
 import logging
 import numpy as np
-import os
 import pytorch_lightning as L
 import random
 import torch
@@ -35,11 +35,11 @@ logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
 class DataModule(L.LightningDataModule):
     def __init__(
         self,
-        bboxes_path,
-        img_prefixes_path,
+        brain_ids,
+        img_paths_json,
         swc_dir,
         batch_size=16,
-        foreground_sampling_rate=0.2,
+        foreground_sampling_rate=0.5,
         n_upds=100,
         n_validate_examples=200,
         patch_shape=(64, 64, 64),
@@ -49,14 +49,14 @@ class DataModule(L.LightningDataModule):
 
         # Instance attributes
         self.batch_size = batch_size
+        self.brain_ids = brain_ids
         self.foreground_sampling_rate = foreground_sampling_rate
         self.n_upds = n_upds
         self.n_validate_examples = n_validate_examples
         self.patch_shape = patch_shape
 
         # Paths
-        self.bboxes_path = bboxes_path
-        self.img_prefixes_path = img_prefixes_path
+        self.img_paths_json = img_paths_json
         self.swc_dir = swc_dir
 
     def prepare_data(self):
@@ -65,8 +65,8 @@ class DataModule(L.LightningDataModule):
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
             self.train_dataset, self.val_dataset = init_datasets(
-                self.bboxes_path,
-                self.img_prefixes_path,
+                self.brain_ids,
+                self.img_paths_json,
                 self.swc_dir,
                 self.patch_shape,
                 self.n_validate_examples,
@@ -93,39 +93,33 @@ class TrainDataset(Dataset):
         patch_shape,
         transform,
         anisotropy=(0.748, 0.748, 1.0),
-        foreground_sampling_rate=0.2,
+        boundary_buffer=4000,
+        foreground_sampling_rate=0.5,
     ):
         # Call parent class
         super(TrainDataset, self).__init__()
 
         # Class attributes
         self.anisotropy = anisotropy
+        self.boundary_buffer = boundary_buffer
         self.foreground_sampling_rate = foreground_sampling_rate
         self.patch_shape = patch_shape
         self.swc_reader = Reader()
         self.transform = transform
 
         # Data structures
-        self.bboxes = dict()
         self.foreground = dict()
         self.imgs = dict()
 
-    def ingest_img(self, brain_id, img_prefix, bbox, swc_pointer=None):
-        # Adjust bbox
-        for i in range(3):
-            bbox["min"][i] += self.patch_shape[i]
-            bbox["max"][i] -= self.patch_shape[i]
-
-        # Store result
-        self.bboxes[brain_id] = bbox
+    def ingest_img(self, brain_id, img_path, swc_pointer):
         self.foreground[brain_id] = self.ingest_swcs(swc_pointer)
-        self.imgs[brain_id] = img_util.read(img_prefix)
+        self.imgs[brain_id] = img_util.read(img_path)
 
     def ingest_swcs(self, swc_pointer):
         if swc_pointer:
             # Initializations
             swc_dicts = self.swc_reader.read(swc_pointer)
-            n_points = np.sum([len(swc_dict["xyz"]) for swc_dict in swc_dicts])
+            n_points = np.sum([len(d["xyz"]) for d in swc_dicts])
 
             # Extract foreground voxels
             if n_points > 0:
@@ -169,19 +163,15 @@ class TrainDataset(Dataset):
             return tuple(self.foreground[brain_id][idx] + shift)
         else:
             voxel = list()
-            for i in range(3):
-                lower = self.bboxes[brain_id]["min"][i]
-                upper = self.bboxes[brain_id]["max"][i]
-                voxel.append(random.randint(lower, upper))
+            for s in self.imgs[brain_id].shape[2::]:
+                upper = s - self.boundary_buffer
+                voxel.append(random.randint(self.boundary_buffer, upper))
             return tuple(voxel)
 
     # --- Helpers ---
     def get_patch(self, brain_id, voxel):
         s, e = img_util.get_start_end(voxel, self.patch_shape)
-        patch = self.imgs[brain_id][
-            0, 0, s[0]: e[0], s[1]: e[1], s[2]: e[2]
-        ]
-        return patch
+        return self.imgs[brain_id][0, 0, s[0]: e[0], s[1]: e[1], s[2]: e[2]]
 
     def to_voxels(self, xyz_arr):
         for i in range(3):
@@ -220,8 +210,8 @@ class ValidateDataset(Dataset):
         """
         return len(self.ids)
 
-    def ingest_img(self, brain_id, img_prefix):
-        self.imgs[brain_id] = img_util.open_img(img_prefix)
+    def ingest_img(self, brain_id, img_path):
+        self.imgs[brain_id] = img_util.read(img_path)
 
     def ingest_example(self, brain_id, voxel):
         # Get clean image
@@ -298,27 +288,10 @@ class TrainN2VDataLoader(DataLoader):
                 threads.append(executor.submit(self.dataset.__getitem__, -1))
 
             # Process results
-            masked_patches = np.zeros(
-                (
-                    self.batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
-            patches = np.zeros(
-                (
-                    self.batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
-            masks = np.zeros(
-                (
-                    self.batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
+            shape = (self.batch_size, 1,) + self.patch_shape
+            masked_patches = np.zeros(shape)
+            patches = np.zeros(shape)
+            masks = np.zeros(shape)
             for i, thread in enumerate(as_completed(threads)):
                 masked_patch, patch, mask = thread.result()
                 masked_patches[i, 0, ...] = masked_patch
@@ -367,20 +340,9 @@ class TrainBM4DDataLoader(DataLoader):
                 processes.append(executor.submit(self.dataset.__getitem__, -1))
 
             # Process results
-            noise_patches = np.zeros(
-                (
-                    self.batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
-            clean_patches = np.zeros(
-                (
-                    self.batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
+            shape = (self.batch_size, 1,) + self.patch_shape
+            noise_patches = np.zeros(shape)
+            clean_patches = np.zeros(shape)
             for i, process in enumerate(as_completed(processes)):
                 noise, clean, _ = process.result()
                 noise_patches[i, 0, ...] = noise
@@ -415,27 +377,10 @@ class ValidateN2VDataLoader(DataLoader):
                 threads.append(executor.submit(self.dataset.__getitem__, idx))
 
             # Process results
-            masked_patches = np.zeros(
-                (
-                    batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
-            patches = np.zeros(
-                (
-                    batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
-            masks = np.zeros(
-                (
-                    batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
+            shape = (batch_size, 1,) + self.patch_shape
+            masked_patches = np.zeros(shape)
+            patches = np.zeros(shape)
+            masks = np.zeros(shape)
             for i, thread in enumerate(as_completed(threads)):
                 masked_patch, patch, mask = thread.result()
                 masked_patches[i, 0, ...] = masked_patch
@@ -473,20 +418,9 @@ class ValidateBM4DDataLoader(DataLoader):
                 )
 
             # Process results
-            noise_patches = np.zeros(
-                (
-                    self.batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
-            clean_patches = np.zeros(
-                (
-                    self.batch_size,
-                    1,
-                )
-                + self.patch_shape
-            )
+            shape = (batch_size, 1,) + self.patch_shape
+            noise_patches = np.zeros(shape)
+            clean_patches = np.zeros(shape)
             mn_mxs = np.zeros((self.batch_size, 2))
             for i, process in enumerate(as_completed(processes)):
                 noise, clean, mn_mx = process.result()
@@ -498,17 +432,16 @@ class ValidateBM4DDataLoader(DataLoader):
 
 # --- Helpers ---
 def init_datasets(
-    bboxes_path,
-    img_prefixes_path,
-    swc_dir,
+    brain_ids,
+    img_paths_json,
     patch_shape,
     n_validate_examples,
     foreground_sampling_rate=0.5,
     method="n2v",
+    swc_dict=None
 ):
     # Initializations
-    bboxes = util.read_json(bboxes_path)
-    img_prefixes = util.read_json(img_prefixes_path)
+    img_paths = util.read_json(img_paths_json)
     if method == "n2v":
         transform = N2VManipulate()
     elif method == "bm4d":
@@ -524,11 +457,16 @@ def init_datasets(
     )
 
     # Load data
-    for brain_id, bbox in tqdm(bboxes.items(), desc="Load Data"):
-        img_prefix = img_prefixes[brain_id] + str(0)
-        swc_pointer = os.path.join(swc_dir, brain_id)
-        train_dataset.ingest_img(brain_id, img_prefix, bbox, swc_pointer)
-        val_dataset.ingest_img(brain_id, img_prefix)
+    for brain_id in tqdm(brain_ids, desc="Load Data"):
+        img_path = img_paths[brain_id] + str(0)
+        if swc_dict:
+            swc_pointer = deepcopy(swc_dict)
+            swc_pointer["path"] += f"/{brain_id}/world"
+        else:
+            swc_pointer = None
+
+        train_dataset.ingest_img(brain_id, img_path, swc_pointer)
+        val_dataset.ingest_img(brain_id, img_path)
 
     # Generate validation examples
     for _ in range(n_validate_examples):
@@ -539,4 +477,18 @@ def init_datasets(
 
 
 def to_tensor(arr):
+    """
+    Converts a numpy array to a tensor.
+
+    Parameters
+    ----------
+    arr : numpy.ndarray
+        Array to be converted.
+
+    Returns
+    -------
+    torch.Tensor
+        Array converted to tensor.
+
+    """
     return torch.tensor(arr, dtype=torch.float)
